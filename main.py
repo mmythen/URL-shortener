e80b0c397b857315f401a89cbe406bc44cba25f0
mmythen <mjmythen@gmail.com>
2026-05-21 19:05:14 -0700

    added redirect functionality from short url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel
 from db import get_db, init_db
 from base62 import encode, decode
@@ -10,7 +11,7 @@ init_db()
 class ShortenRequest(BaseModel):
     url : str
 
-
+# LONG URL -> SHORT URL
 @app.post('/shorten')
 def shorten_url(request: ShortenRequest):
     url = request.url
@@ -37,3 +38,23 @@ def shorten_url(request: ShortenRequest):
     conn.close()
 
     return { 'short_url': f'http://localhost:8000/{short_code}' }
+
+# SHORT URL -> LONG URL
+@app.get('/{short_code}')
+def redirect(short_code: str):
+    conn = get_db()
+
+    row = conn.execute(
+        'SELECT * FROM urls WHERE short_code = ?', (short_code,)
+    ).fetchone()
+
+    if not row:
+        raise HTTPException(status_code=404, detail='Short code not found')
+    
+    conn.execute(
+        'UPDATE urls SET click_count = click_count + 1 WHERE short_code = ?', (short_code, )
+    )
+    conn.commit()
+    conn.close()
+
+    return RedirectResponse(url=row['long_url'], status_code=302)
