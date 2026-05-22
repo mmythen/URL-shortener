9330f5969a81dd440b46f1848d4327ca3bb3f441
mmythen <mjmythen@gmail.com>
2026-05-21 18:37:17 -0700

    moved dp to seperate python file

---
+++ db.py
@@ -0,0 +1,22 @@
+import sqlite3
+import base62
+
+def get_db():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row #change tuples to sqlite rows
+    return conn
+
+def init_db():
+    conn = get_db()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS urls (
+            id          INTEGER PRIMARY KEY AUTOINCREMENT,
+            short_code  TEXT UNIQUE,
+            long_url    TEXT NOT NULL,
+            click_count INTEGER DEFAULT 0,
+            created_at  TEXT DEFAULT (datetime('now'))
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
