51eadb4577a25ff12e656f5723c979d0da1a2dd0
mmythen <mjmythen@gmail.com>
2026-05-21 18:32:32 -0700

    refactored encoding/decoding to seperate file

---
+++ base62.py
@@ -0,0 +1,20 @@
+import string
+CHARSET = string.digits + string.ascii_lowercase + string.ascii_uppercase
+
+
+def encode(num):
+    if num == 0:
+        return CHARSET[0]
+    arr = []
+    while num:
+        num, rem =  divmod(num, 62)
+        arr.append(CHARSET[rem])
+
+    arr.reverse()
+    return ''.join(arr)
+
+def decode(s):
+    num = 0
+    for char in s:
+        num = num * 62 + CHARSET.index(char)
+    return num
--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-import string
-CHARSET = string.digits + string.ascii_lowercase + string.ascii_uppercase
+import sqlite3
+import base62
 
-def encode_base62(num):
-    if num == 0:
-        return CHARSET[0]
-    arr = []
-    while num:
-        num, rem =  divmod(num, 62)
-        arr.append(CHARSET[rem])
-
-    arr.reverse()
-    return ''.join(arr)
-
-def decode_base62(s):
-    num = 0
-    for char in s:
-        num = num * 62 + CHARSET.index(char)
-    return num
+print(base62.encode(9999))
 
